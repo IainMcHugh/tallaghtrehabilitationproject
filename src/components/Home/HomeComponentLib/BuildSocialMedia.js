45f1c8fcc9b6482b0bc688cf4c6434d9b0af4da8
IainMcHugh <46798029+IainMcHugh@users.noreply.github.com>
2020-07-14 20:13:09 +0100

    Redeploy

--- src/components/Home/HomeComponentLib/BuildSocialMedia.js
+++ src/components/Home/HomeComponentLib/BuildSocialMedia.js
@@ -22,7 +22,7 @@ function BuildSocialMedia() {
                 <img
                   className="video-play-button"
                   src={require("../../../images/play_button_out.png")}
-                  alt="Play Button"
+                  alt="Play-Button"
                 />
               </div>
             </div>
