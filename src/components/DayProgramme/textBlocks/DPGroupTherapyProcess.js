5c66cc1c51f651b4f01ba2e6e122c292824976f8
IainMcHugh <46798029+IainMcHugh@users.noreply.github.com>
2020-07-14 20:50:48 +0100

    Updates

--- src/components/DayProgramme/textBlocks/DPGroupTherapyProcess.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default [
-    `There are 3 process groups facilitated on a weekly basis. Participants are
-    encouraged to share their experiences and support each other in a peer led
-    process in a caring and supportive enviroment.`
-];
--- src/components/DayProgramme/textBlocks/DPRecoveryAwareness.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-export default [
-  `This module addresses people who are in the early stages of recovery. The
-    approach is very practical and heightens the participant’s knowledge and
-    awareness of their addiction.`,
-];
