53f2255733861e239e6edf09b882661632b43f04
IainMcHugh <iamchugh@tcd.ie>
2021-02-13 16:22:42 +0000

    fix unique key console error

--- src/components/Contact/Toolkit/Section.jsx
+++ src/components/Contact/Toolkit/Section.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
 const Section = ({ title, subheading, data }) => (
-  <div className="contact-box">
+  <div className="contact-box" id="about-subtext-show">
     {title && <h2 className="contact-heading">{title}</h2>}
     {subheading && <h3>{subheading}</h3>}
-    <h4 className="contact-subtext-show">{data}</h4>
+    <div className="contact-subtext-show">
+      {data.map((value, index) => (
+        <h4 key={index}>{value}</h4>
+      ))}
+    </div>
   </div>
 );
 
